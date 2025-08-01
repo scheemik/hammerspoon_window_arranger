4aa56a3ebd1f12351e73107a9af1f0fba2b127bc
GreyAudron <mikhail.schee@mail.utoronto.ca>
2025-08-01 15:13:47 -0400

    feat: add initialization script to load other modules

---
+++ init.lua
@@ -0,0 +1,3 @@
+-- Import the functions from the modules
+local dev_functions = require("hammerspoon_window_arranger/dev_functions")
+local work_window_arranger = require("hammerspoon_window_arranger/work_windows")
